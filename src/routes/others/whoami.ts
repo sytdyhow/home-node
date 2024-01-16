a889c6b21ef2b70c135cece96c20aa614bfecb97
yenish sytdyhow <yenishsytdyhow@gmail.com>
2024-01-16 10:56:03 +0500

    new whoami

--- src/routes/others/whoami.ts
+++ src/routes/others/whoami.ts
@@ -3,6 +3,9 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import LogsDatasource from "../../database/logsdb";
 import { PairsEntity } from "../../entities/pairs_entity";
 import { RulesEntity } from "../../entities/rules-entity";
+import { getRepository } from "typeorm";
+import { UsersEntity } from "../../entities/users-entity";
+import { RolesEntity } from "../../entities/roles-entity";
 
 const router = express.Router();
 
@@ -17,28 +20,20 @@ router.get('/whoami', async (req, res) => {
   try {
     const decodedToken = jwt.verify(token, 'system') as JwtPayload;
     const users_id = decodedToken.id;
-   
-    
-      
-      const rule_id = await LogsDatasource
-      .getRepository(PairsEntity)    
-      .createQueryBuilder('pairs_table')
-      .select(['pairs_table.rule_id'])
-      .where('pairs_table.user_id = :id',{id:users_id})
-      .getMany()
 
-     const rulesIds= rule_id.map((rl)=>rl.rule_id)
-    
-      
-      const rules =await LogsDatasource
-      .getRepository(RulesEntity)
-      .createQueryBuilder('rules')
-      .select(['rules.id','rules.rule_name'])
-      .where('rules.id IN(:...ids)',{ids:rulesIds})
-      .getMany()
+    const user = await getRepository(UsersEntity)
+      .createQueryBuilder('users')
+      .select(['users.id', 'users.username'])
+      .where('users.id = :id', { id: users_id })
+      .getOne();
 
+    const role = await getRepository(RolesEntity)
+      .createQueryBuilder('roles')
+      .innerJoin('users_roles', 'rol', 'roles.id = rol.roles_id')
+      .where('rol.users_id = :id', { id: users_id })
+      .getMany();
 
-    return res.json({ rules });
+    return res.json({ user, role });
   } catch (error) {
     return res.status(401).json({ error: "Invalid token" });
   }
