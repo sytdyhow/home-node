6cbb75eb74ee42010f3e65fe077b01e569504aa2
yenish sytdyhow <yenishsytdyhow@gmail.com>
2024-02-02 13:10:12 +0500

    auth

--- src/routes/roles_crud/get_role.ts
+++ src/routes/roles_crud/get_role.ts
@@ -1,12 +1,54 @@
 import express from "express";
 import { RolesEntity } from "../../entities/roles-entity";
+import { getRepository } from "typeorm";
+import { UsersEntity } from "../../entities/users-entity";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const router =express.Router();
 
 
 router.get ('/roles',async (req,res)=>{
-    const roles =await RolesEntity.find();
-    return res.send({results:roles})
 
-})
+    const auth = req.headers.authorization;
+    const token = auth?.split(" ")[1];
+  
+    if (!token) {
+      return res.status(401).json({ error: "Missing token" });
+    }
+  
+    try {
+      const decodedToken = jwt.verify(token, 'system') as JwtPayload;
+      const users_id = decodedToken.id;
+  
+  
+        const role_id = await getRepository(UsersEntity)
+        .createQueryBuilder('users')
+        .select(['users.roles_id'])
+        .where('users.id = :id', { id: users_id })
+        .getOne();
+  
+     
+      const role = await getRepository(RolesEntity)
+        .createQueryBuilder('roles')
+        .leftJoin('users', 'rol', 'roles.id = rol.roles_id')
+        .where('rol.id = :id', { id: users_id })
+        .getMany();
+        
+        const roleId=role[0].id;
+        const rolename=role[0].name
+
+
+        if(role_id?.roles_id===roleId && rolename==="admin"){
+            const roles =await RolesEntity.find();
+            return res.send({results:roles})
+        
+        }
+
+      return res.json({results: []});
+    } catch (error) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+   
+});
+
 export { router as getRolerouter}
--- src/routes/users_crud/get_user.ts
+++ src/routes/users_crud/get_user.ts
@@ -3,10 +3,46 @@
 import express from "express";
 import { UsersEntity } from "../../entities/users-entity";
 import { RolesEntity } from "../../entities/roles-entity";
+import { getRepository } from "typeorm";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get('/users', async (req, res) => {
+
+  const auth = req.headers.authorization;
+  const token = auth?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Missing token" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, 'system') as JwtPayload;
+    const users_id = decodedToken.id;
+
+
+      const role_id = await getRepository(UsersEntity)
+      .createQueryBuilder('users')
+      .select(['users.roles_id'])
+      .where('users.id = :id', { id: users_id })
+      .getOne();
+
+   
+
+
+    const role = await getRepository(RolesEntity)
+      .createQueryBuilder('roles')
+      .leftJoin('users', 'rol', 'roles.id = rol.roles_id')
+      .where('rol.id = :id', { id: users_id })
+      .getMany();
+      
+      const roleId=role[0].id;
+      const rolename=role[0].name
+
+
+      if(role_id?.roles_id===roleId && rolename==="admin"){
+       
   const users = await UsersEntity.find({
     // where: {
     //   is_active: true
@@ -16,8 +52,6 @@ router.get('/users', async (req, res) => {
 
   const roles = await RolesEntity.find()
 
-  
-  
   const activeUsers = users.map((user) => {
     const { password, ...userWithoutPassword } = user;
     const role = roles.find((role)=>userWithoutPassword.roles_id===role.id)
@@ -32,6 +66,16 @@ router.get('/users', async (req, res) => {
   });
 
   return res.send({results: activeUsers});
+
+      }
+
+    return res.json({results: []});
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+  
 });
+
+
 
 export { router as getUserRouter };
